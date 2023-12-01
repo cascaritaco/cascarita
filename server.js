73ac9daab391143a7de2d9f0a8a8a43045a4dd42
Saul Reyes <reyessaul796@gmail.com>
2023-12-01 09:45:27 -0800

    wip creating a team via url

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
-const sequelize = reuire('sequelize');
+const sequelize = require('sequelize');
+const teamController = require("../nodeMVC/controllers/teamController")
 const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+
+app.post('/create/:name', teamController.createTeam);
+
+app.get('/create/:name', (req, res) => {
+  res.send('GET request received for creating a team with name ' + req.params.name);
 });
 
 app.listen(port, () => {
