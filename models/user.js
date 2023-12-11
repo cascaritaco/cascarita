5ec745e1de9c2532cc192f22a0f9432fe0c0e412
Saul Reyes <reyessaul796@gmail.com>
2023-12-11 13:02:19 -0800

    create models and migrations to create users, groups and roles tables

---
+++ models/user.js
@@ -0,0 +1,53 @@
+'use strict';
+const Bcrypt = require('bcrypt');
+
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      User.belongsTo(models.Group, { foreignKey: 'groupId' });
+      User.belongsTo(models.Role, { foreignKey: 'roleId' });
+    }
+  }
+  User.init({
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING(64),
+      allowNull: false,
+    },
+    groupId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    roleId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+
+  User.beforeCreate(async (user) => {
+    const saltRounds = 10;
+    user.password = await Bcrypt.hash(user.password, saltRounds);
+  });
+
+  return User;
+};
