17d9acdcaf53f88715e3fa096b3f47ea87a3b126
Saul Reyes <reyessaul796@gmail.com>
2023-12-19 12:43:34 -0800

    chore: lint fix ran on the controllers

--- server/controllers/group.controller.js
+++ server/controllers/group.controller.js
@@ -1,35 +1,35 @@
 "use strict";
-const  { Group } = require("../models");
+const { Group } = require("../models");
 
-const GroupController = function(){
+const GroupController = function () {
+  var createGroup = async function (req, res) {
+    const newGroup = {
+      name: req.body.name,
+    };
 
-    var createGroup = async function(req, res){
+    try {
+      await Group.build(newGroup).validate();
+      const result = await Group.create(newGroup);
 
-        const newGroup  = {
-            name: req.body.name,
-        }
-
-        try {
-            await Group.build(newGroup).validate();
-            const result = await Group.create(newGroup);
-
-            return res.status(201).json({
-                success: true,
-                data: result
-            });
-        } catch (error) {
-            return res.status(400).json({ error: error.message });
-        }
+      return res.status(201).json({
+        success: true,
+        data: result,
+      });
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
     }
+  };
 
-    var _checkUniqueGroupName = async function (inputGroupName) {
-        const existingGroupName = await Group.findOne({where: {name: inputGroupName}})
-        return !existingGroupName;
-    }
+  var _checkUniqueGroupName = async function (inputGroupName) {
+    const existingGroupName = await Group.findOne({
+      where: { name: inputGroupName },
+    });
+    return !existingGroupName;
+  };
 
-    return{
-        createGroup,
-    }
-}
+  return {
+    createGroup,
+  };
+};
 
 module.exports = GroupController();
--- server/controllers/role.controller.js
+++ server/controllers/role.controller.js
@@ -1,30 +1,28 @@
 "use strict";
-const  { Role } = require("../models");
+const { Role } = require("../models");
 
-const RoleController = function(){
+const RoleController = function () {
+  var createRole = async function (req, res) {
+    const newRole = {
+      role_type: req.body.role_type,
+    };
 
-    var createRole = async function(req, res){
+    try {
+      await Role.build(newRole).validate();
+      const result = await Role.create(newRole);
 
-        const newRole  = {
-            role_type: req.body.role_type,
-        }
-
-        try {
-            await Role.build(newRole).validate();
-            const result = await Role.create(newRole);
-
-            return res.status(201).json({
-                success: true,
-                data: result
-            });
-        } catch (error) {
-            return res.status(400).json({ error: error.message });
-        }
+      return res.status(201).json({
+        success: true,
+        data: result,
+      });
+    } catch (error) {
+      return res.status(400).json({ error: error.message });
     }
+  };
 
-    return{
-        createRole,
-    }
-}
+  return {
+    createRole,
+  };
+};
 
 module.exports = RoleController();
