bf583019e17016cf3587cec6219e5a5c97a38da4
Saul Reyes <reyessaul796@gmail.com>
2024-01-06 17:09:40 -0800

    fix: handle cookies and sessions. Handle log in errors by redirecting user

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -3,9 +3,20 @@
 const express = require("express");
 const router = express.Router();
 const UserController = require("../controllers/user.controller");
+const passport = require("passport");
+
+router.get("/loginReactPageHere", (req, res) => {
+  res.json({ message: "Invalid email or password, try again" });
+});
 
 router.post("/register", UserController.registerUser);
 
-router.post("/login", UserController.logInUser);
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/user/loginReactPageHere",
+  }),
+  UserController.logInUser
+);
 
 module.exports = router;
