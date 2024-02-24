0d18c44e1598bc712ba4a25704c9938a6b5e97d2
Jen-464 <jenlopez464@gmail.com>
2024-02-23 20:17:09 -0800

    feat: Updated Team Model

--- server/models/team.js
+++ server/models/team.js
@@ -1,28 +1,36 @@
 "use strict";
 const { Model } = require("sequelize");
-
 module.exports = (sequelize, DataTypes) => {
   class Team extends Model {
-    static logMessage(message) {
-      console.log(message);
-      return "";
-    }
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Team.belongsTo(models.Group, { foreignKey: "group_id" });
     }
   }
   Team.init(
     {
-      name: DataTypes.STRING,
+      group_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      team_logo: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
     {
       sequelize,
       modelName: "Team",
+      createdAt: "created_at",
+      updatedAt: "updated_at",
     }
   );
   return Team;
