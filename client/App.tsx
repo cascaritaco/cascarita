461a73eec15b1ab258d67518eddd629ccf4e0d4f
Armando Banuelos <abanuelo@Armandos-MBP.lan>
2024-03-13 14:02:49 -0700

    fix: addressing code rabbit first pass comments

--- client/App.tsx
+++ client/App.tsx
@@ -1,17 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard/Dashboard";
-// import About from "./pages/About/About";
-// import NavBar from "./components/NavBar/NavBar";
 
 const App = () => {
   return (
-    <>
-      {/* <NavBar /> */}
+    <div>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        {/* <Route path="/about" element={<About />} /> */}
       </Routes>
-    </>
+    </div>
   );
 };
 
