f5c6deb94f8fb7263204f390c9cb40992dc4066a
Jesus Gomez <gmjesus95@gmail.com>
2024-03-19 23:59:59 -0700

    feat: Created the controller and the createLeague/getLeagueByGroupId routes for the League table

--- server/app.js
+++ server/app.js
@@ -17,6 +17,7 @@ const PlayerRoutes = require("./routes/player.routes");
 const SeasonRoutes = require("./routes/season.routes");
 const Middlewares = require("./middlewares");
 const LeagueRoutes = require("./routes/league.routes");
+
 const Middlewares = require("./middlewares");
 
 const app = express();
