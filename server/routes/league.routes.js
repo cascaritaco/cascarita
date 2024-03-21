92984a69cf844eee329735168d6ec661dc3e4bf6
Jesus Gomez <gmjesus95@gmail.com>
2024-03-21 16:35:42 -0700

    feat: Added the update and delete functionality to League controller and routes

--- server/routes/league.routes.js
+++ server/routes/league.routes.js
@@ -6,5 +6,7 @@ const LeagueController = require("../controllers/league.controller");
 
 router.post("/create", LeagueController.createLeague);
 router.get("/getByGroupId", LeagueController.getLeagueByGroupId);
+router.post("/update", LeagueController.updateLeague);
+router.delete("/delete", LeagueController.deleteLeague);
 
 module.exports = router;
