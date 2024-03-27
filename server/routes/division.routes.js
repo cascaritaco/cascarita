d0b71e08987116e982a3e8f6e3fe06dac11fd10d
Humberto <hdkaratechop@gmail.com>
2024-03-26 20:03:58 -0700

    Refactor division routes to use DivisionController

--- server/routes/division.routes.js
+++ server/routes/division.routes.js
@@ -1,26 +1,14 @@
 "use strict";
 const express = require('express');
-const divisionController = require('../controllers/division.controller');
+const DivisionController = require('../controllers/division.controller');
 
 const router = express.Router();
 
-// Route for creating a new division
-router.post('/', divisionController.create);
-
-// Route for updating a division
-router.put('/:id', divisionController.update);
-
-// Route for archiving divisions by GroupID
-router.put('/archive/:groupId', divisionController.archiveByGroupId);
-
-// Route for getting divisions by GroupID
-router.get('/group/:groupId', divisionController.getByGroupId);
-
-// Route for getting deleted divisions by GroupID from DivisionsHistory
-
-router.get('/history/group/:groupId', divisionController.getDeletedDivisionsByGroupId);
-
-// Route for deleting a division by ID
-router.delete('/:id', divisionController.delete);
+router.post('/', DivisionController.create);
+router.put('/:id', DivisionController.update);
+router.put('/archive/:groupId', DivisionController.archiveByGroupId);
+router.get('/group/:groupId', DivisionController.getByGroupId);
+router.get('/history/group/:groupId', DivisionController.getDeletedDivisionsByGroupId);
+router.delete('/:id', DivisionController.delete);
 
 module.exports = router;
