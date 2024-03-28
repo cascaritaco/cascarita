6e6845919c375a1a1e8945135aed85a7d128bcea
Jesus Gomez <gmjesus95@gmail.com>
2024-03-28 16:14:05 -0700

    feat: added a duplicate edge case unit test for league

--- server/__tests__/routeTests/league.routes.test.js
+++ server/__tests__/routeTests/league.routes.test.js
@@ -42,6 +42,7 @@ describe("League Routes", () => {
     await testDb.League.create({ group_id: groupM.id, name: "Salinas" });
 
     const response = await request(app)
+<<<<<<< HEAD
       .post("/league/")
       .send({ group_id: groupM.id, name: "Salinas" });
 
