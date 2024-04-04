0473aaba58fec3a72acf85b977408de6636a3732
Saul Reyes <reyessaul796@gmail.com>
2024-04-04 16:06:28 -0700

    test: intergration test for get route and controller

--- server/__tests__/routeTests/group.routes.test.js
+++ server/__tests__/routeTests/group.routes.test.js
@@ -4,86 +4,74 @@ const request = require("supertest");
 const express = require("express");
 const GroupController = require("../../controllers/group.controller");
 const GroupRoutes = require("../../routes/group.routes");
+const Middlewares = require("../../middlewares");
+const TestDb = require("../../models");
 const app = express();
 app.use(express.json());
 app.use("/group", GroupRoutes);
+app.use(Middlewares.errorHandler);
 
-jest.mock("../../controllers/group.controller", () => ({
-  createGroup: jest.fn(),
-}));
+const sampleGroup = {
+  name: "Sample Group",
+  street_address: "123 Main Street",
+  city: "Sample City",
+  state: "CA",
+  zip_code: "12345",
+  logo_url: "https://example.com/logo",
+};
 
-describe("Group Routes", () => {
-  it("should handle POST /create", async () => {
-    GroupController.createGroup.mockImplementation((req, res) => {
-      res.status(201).json({ success: true, data: { name: "TestGroup" } });
-    });
+const commonBeforeEach = async function() {
+  await TestDb.Group.sync();
+};
 
+const commonAfterEach = async function() {
+  await TestDb.Group.destroy({ where: {} });
+};
+
+describe("GET/ Group routes", () => {
+  beforeEach(commonBeforeEach);
+  afterEach(commonAfterEach);
+
+  it("successful GET of group information", async () => {
+    const coolGroup = await TestDb.Group.create(sampleGroup);
     const response = await request(app)
-      .post("/group/create")
-      .send({ name: "TestGroup" });
+      .get(`/group/${coolGroup.id}`)
+
+    expect(response.status).toBe(200);
+    expect(response.body.data).toEqual(expect.objectContaining({
+      name: "Sample Group",
+      city: "Sample City"
+    }));
+  });
+
+  it("returns an error when attempting to retrieve non-existent group", async () => {
+    const missingGroup = {id: 12345}
+    const response = await request(app)
+      .get(`/group/${missingGroup.id}`)
+
+    expect(response.status).toBe(500);
+    expect(response.body).toEqual(expect.objectContaining({
+      message: "Group with given ID was not found",
+    }));
+  });
+});
+
+describe("POST/ Group routes", () => {
+  beforeEach(commonBeforeEach);
+  afterEach(commonAfterEach);
+
+  it("successful POST when creating a new group", async () => {
+    const response = await request(app)
+      .post("/group/").send(sampleGroup)
 
     expect(response.status).toBe(201);
-    expect(response.body).toEqual({
-      success: true,
-      data: { name: "TestGroup" },
-    });
+    expect(response.body.data).toEqual(expect.objectContaining({
+      name: "Sample Group",
+      city: "Sample City"
+    }));
   });
+});
 
-  it("should handle POST /create with null name", async () => {
-    GroupController.createGroup.mockImplementation((req, res) => {
-      res.status(400).json({
-        error: "Validation Error",
-        details: [
-          {
-            field: "name",
-            message: "notNull Violation: Group.name cannot be null",
-          },
-        ],
-      });
-    });
-
-    const response = await request(app)
-      .post("/group/create")
-      .send({ name: null });
-
-    expect(response.status).toBe(400);
-    expect(response.body).toEqual({
-      error: "Validation Error",
-      details: [
-        {
-          field: "name",
-          message: "notNull Violation: Group.name cannot be null",
-        },
-      ],
-    });
-  });
-
-  it("should handle POST /create with non unique name", async () => {
-    GroupController.createGroup.mockImplementation((req, res) => {
-      res.status(400).json({
-        error: "Validation Error",
-        details: [
-          {
-            field: "name",
-            message: "name must be unique",
-          },
-        ],
-      });
-    });
-
-    const response = await request(app)
-      .post("/group/create")
-      .send({ name: "Soccer Central" });
-
-    expect(response.status).toBe(400);
-    expect(response.body).toEqual({
-      error: "Validation Error",
-      details: [
-        {
-          field: "name",
-          message: "name must be unique",
-        },
-      ],
-    });
-  });
+afterAll(async () => {
+  await TestDb.sequelize.close();
 });
