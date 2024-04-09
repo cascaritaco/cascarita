bc17ab9e3c63ac2bfc74b8cae89a082f6435d7d8
Leo Villalobos <villaleobos@gmail.com>
2024-04-09 00:18:11 -0700

    fix!: remove season and group model relationships

--- server/models/group.js
+++ server/models/group.js
@@ -8,11 +8,6 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
-      Group.hasMany(models.Season, {
-        foreignKey: "group_id",
-        sourceKey: "id",
-      });
       Group.hasMany(models.League, {
         foreignKey: "group_id",
         sourceKey: "id",
--- server/models/season.js
+++ server/models/season.js
@@ -7,13 +7,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
-      Season.belongsTo(models.Group, {
-        foreignKey: "group_id",
-        targetKey: "id",
-      });
-    }
+    static associate(models) { }
   }
   Season.init(
     {
