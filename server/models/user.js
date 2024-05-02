ca367c17e233a5460e25bf877a8ac64ae91a03f1
Armando Banuelos <abanuelo@Armandos-MBP.lan>
2024-05-01 22:53:01 -0700

    fix: adding back associations for user model

--- server/models/user.js
+++ server/models/user.js
@@ -10,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // User.belongsTo(models.Group, { foreignKey: "group_id" });
-      // User.belongsTo(models.Role, { foreignKey: "role_id" });
+      User.belongsTo(models.Group, { foreignKey: "group_id" });
+      User.belongsTo(models.Role, { foreignKey: "role_id" });
     }
   }
   User.init(
