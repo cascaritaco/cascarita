9456a0309518e8223e923fa0a9fe99ac15ed8b7d
Armando Banuelos <abanuelo@Armandos-MBP.lan>
2024-05-04 14:29:41 -0700

    feat: updating css for login screen

--- client/pages/Login/Login.tsx
+++ client/pages/Login/Login.tsx
@@ -1,6 +1,6 @@
 import styles from "./Login.module.css";
 import LogoWhite from "../../assets/logoWhite.svg";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useAuth } from "../../components/AuthContext/AuthContext";
 import { useNavigate } from "react-router-dom";
 
@@ -10,6 +10,15 @@ const Login = () => {
   const [email, setEmail] = useState("");
   const [fail, setFail] = useState(false);
   const [password, setPassword] = useState("");
+
+  useEffect(() => {
+    // Set the background color of the body to white
+    document.body.style.backgroundColor = "#ffffff";
+    // Clean up the style when the component unmounts
+    return () => {
+      document.body.style.backgroundColor = ""; // Restore default background color
+    };
+  }, []);
 
   const handleLogin = async () => {
     try {
@@ -24,24 +33,26 @@ const Login = () => {
   };
 
   return (
-    <div className={styles.login}>
+    <div className={styles.login} data-name="login">
       <div className={styles.left}>
-        <LogoWhite className={styles.logo} />
+        <div className={styles.logoContainer}>
+          <LogoWhite className={styles.logo} />
+        </div>
         <h1>Welcome to cascarita!</h1>
       </div>
       <div className={styles.right}>
         <h3 className={styles.loginHeader}>Welcome back!</h3>
         {fail ? <p>Login failed, please try again.</p> : <></>}
-        <p className={styles.loginSubHeaders}>Email or username</p>
+        <p className={styles.loginSubHeaders}>E-mail</p>
         <input
-          placeholder="email or username"
+          placeholder="Type your e-mail"
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <p>Password</p>
         <input
-          placeholder="password"
+          placeholder="Type your password"
           type="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
