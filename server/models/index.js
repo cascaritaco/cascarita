96a8ed63a1adf71a0d95e04c5932d16890d7cefd
Armando Banuelos <abanuelo@Armandos-MBP.lan>
2024-05-04 17:15:05 -0700

    fix: adding some changes

--- server/models/index.js
+++ server/models/index.js
@@ -31,13 +31,19 @@ function updateConfigFile() {
 const db = {};
 updateConfigFile();
 let sequelize;
+
 const dbConfig = require(__dirname + "/../config/config.json")[env];
-sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  dbConfig
-);
+
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], dbConfig);
+} else {
+  sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    dbConfig
+  );
+}
 
 sequelize
   .authenticate()
