4f9487bda003e09cec0797a48dc85c1a7f6ef63f
saulRM10 <43281985+saulRM10@users.noreply.github.com>
2024-05-16 16:15:12 -0700

    Chore/update group return response (#57)

--- server/controllers/group.controller.js
+++ server/controllers/group.controller.js
@@ -2,8 +2,7 @@
 const { Group } = require("../models");
 
 const GroupController = function () {
-
-  var _getGroup = async function(id){
+  var _getGroup = async function (id) {
     let currentGroup = await Group.findOne({
       where: {
         id: id,
@@ -11,17 +10,14 @@ const GroupController = function () {
     });
 
     if (!currentGroup) {
-      throw new Error( "Group with given ID was not found" );
+      throw new Error("group with given id was not found");
     } else return currentGroup;
-  }
+  };
 
   var getGroupById = async function (req, res, next) {
     try {
-      return res.status(200).json({
-        success: true,
-        data: await _getGroup(req.params['id']),
-      });
-
+      const group = await _getGroup(req.params["id"]);
+      return res.status(200).json(group);
     } catch (error) {
       next(error);
     }
@@ -36,37 +32,28 @@ const GroupController = function () {
       zip_code: req.body.zip_code,
       logo_url: req.body.logo_url,
     };
-    
+
     try {
       await Group.build(newGroup).validate();
       const result = await Group.create(newGroup);
-
-      return res.status(201).json({
-        success: true,
-        data: result,
-      });
+      return res.status(201).json(result);
     } catch (error) {
-        next(error);
+      next(error);
     }
   };
 
   var updateGroup = async function (req, res, next) {
     try {
-      let currentGroup = await _getGroup(req.params['id'])
+      let currentGroup = await _getGroup(req.params["id"]);
 
-      Object.keys(req.body).forEach(key => {
+      Object.keys(req.body).forEach((key) => {
         currentGroup[key] = req.body[key] ? req.body[key] : currentGroup[key];
       });
-  
+
       await currentGroup.validate();
       await currentGroup.save();
-  
-      return res.status(200).json({
-        success: true,
-        data: currentGroup,
-      });
 
-
+      return res.status(200).json(currentGroup);
     } catch (error) {
       next(error);
     }
