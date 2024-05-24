a0f87e42e5f0f76e50c94150957dd546b1eed9fd
Saul Reyes <reyessaul796@gmail.com>
2024-05-23 18:34:26 -0700

    feat: update index js to use env for everything esp to set up db connections

--- server/models/index.js
+++ server/models/index.js
@@ -8,45 +8,30 @@ require("dotenv").config();
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || "development";
 
-const configFilePath = path.join(__dirname, "../config/config.json");
-const config = require(configFilePath);
-
-const configManager = {
-  updateConfigFile: () => {
-    const environmentConfig = {
-      username: process.env.DB_USERNAME || "root",
-      password: process.env.DB_PASSWORD || null,
-      database: process.env.DB_NAME || "test_db",
-      host: process.env.DB_HOST || "127.0.0.1",
-      dialect: process.env.DB_DIALECT || "mysql",
-      port: parseInt(process.env.DB_PORT, 10) || 3306,
-    };
-    const newConfig = {
-      ...config,
-    };
-
-    newConfig[env] = environmentConfig;
-    fs.writeFileSync(configFilePath, JSON.stringify(newConfig, null, 2));
-  },
-};
-
 const db = {};
-// NOTE: we will ignore the testing as its configuration is present within config.json
-if (env !== "testing") {
-  configManager.updateConfigFile();
-}
 
 let sequelize;
-const dbConfig = require(__dirname + "/../config/config.json")[env];
-
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], dbConfig);
-} else {
+if (env === "development" || env === "production") {
   sequelize = new Sequelize(
-    dbConfig.database,
-    dbConfig.username,
-    dbConfig.password,
-    dbConfig
+    process.env.DB_NAME,
+    process.env.DB_USERNAME,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      dialect: process.env.DB_DIALECT,
+      port: parseInt(process.env.DB_PORT, 10),
+    }
+  );
+} else if (env === "testing") {
+  sequelize = new Sequelize(
+    process.env.TEST_DB_NAME,
+    process.env.TEST_DB_USERNAME,
+    process.env.TEST_DB_PASSWORD,
+    {
+      host: process.env.TEST_DB_HOST,
+      dialect: process.env.TEST_DB_DIALECT,
+      port: parseInt(process.env.TEST_DB_PORT, 10),
+    }
   );
 }
 
