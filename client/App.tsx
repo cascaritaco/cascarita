b6c1b3093c472416f52e95d837ca4e253d70d77b
Armando Banuelos <abanuelo@Armandos-MacBook-Pro.local>
2024-05-25 16:53:09 -0700

    tem removing app.tsx auth

--- client/App.tsx
+++ client/App.tsx
@@ -5,24 +5,24 @@ import Schedule from "./pages/Schedule/Schedule";
 import Forms from "./pages/Forms/Forms";
 import Settings from "./pages/Settings/Settings";
 import Login from "./pages/Login/Login";
-import { AuthProvider } from "./components/AuthContext/AuthContext";
+// import { AuthProvider } from "./components/AuthContext/AuthContext";
 import Layout from "./components/Layout/Layout";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/schedule" element={<Schedule />} />
-          <Route path="/forms" element={<Forms />} />
-          <Route path="/settings" element={<Settings />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Layout>
-    </AuthProvider>
+    // <AuthProvider>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/users" element={<Users />} />
+        <Route path="/schedule" element={<Schedule />} />
+        <Route path="/forms" element={<Forms />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Layout>
+    // </AuthProvider>
   );
 };
 
