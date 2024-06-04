0ea8350407f06e5f826d4053aaddd86d9e7a6b0e
Saul Reyes <reyessaul796@gmail.com>
2024-06-04 12:45:06 -0700

    feat: end point to get divisions which are not linked to a session

--- server/routes/league.routes.js
+++ server/routes/league.routes.js
@@ -4,10 +4,12 @@ const express = require("express");
 const router = express.Router();
 const LeagueController = require("../controllers/league.controller");
 const SeasonController = require("../controllers/season.controller");
+const DivisionController = require("../controllers/division.controller");
 
 router.post("/", LeagueController.createLeague);
 router.get("/:id", LeagueController.getLeagueByGroupId);
 router.get("/:id/seasons", SeasonController.getTeamsByLeagueId);
+router.get("/:id/divisions", DivisionController.getUnassociatedDivisions);
 router.patch("/:id", LeagueController.updateLeague);
 router.delete("/:id", LeagueController.deleteLeague);
 
