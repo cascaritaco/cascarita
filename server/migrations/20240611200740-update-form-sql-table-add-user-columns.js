07bf3af02a12fdebea444dfb29efbbc124eba1f3
saulRM10 <43281985+saulRM10@users.noreply.github.com>
2024-06-13 22:16:01 -0700

    Feature: Add GET Form endpoints  (#80)

---
+++ server/migrations/20240611200740-update-form-sql-table-add-user-columns.js
@@ -0,0 +1,49 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+
+    try {
+      await queryInterface.addColumn("Forms", "created_by", {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+        transaction,
+      });
+      await queryInterface.addColumn("Forms", "updated_by", {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+        transaction,
+      });
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.removeColumn("Forms", "created_by", { transaction });
+      await queryInterface.removeColumn("Forms", "updated_by", { transaction });
+      await transaction.commit();
+    } catch (error) {
+      await transaction.rollback();
+      throw error;
+    }
+  },
+};
--- server/routes/group.routes.js
+++ server/routes/group.routes.js
@@ -6,11 +6,13 @@ const GroupController = require("../controllers/group.controller");
 const DivisionController = require("../controllers/division.controller");
 const FieldController = require("../controllers/field.controller");
 const LeagueController = require("../controllers/league.controller");
+const FormController = require("../controllers/form.controller");
 
 router.get("/:id", GroupController.getGroupById);
 router.get("/:id/divisions", DivisionController.getByGroupId);
 router.get("/:id/fields", FieldController.getFieldByGroupId);
 router.get("/:id/leagues", LeagueController.getLeagueByGroupId);
+router.get("/:id/forms", FormController.getAllForms);
 router.post("/", GroupController.createGroup);
 router.patch("/:id", GroupController.updateGroup);
 
