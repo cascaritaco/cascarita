ae0d67deab49687ec61d5ecc014e3dcccbe507df
Eli Manzo <96manzo.eli@gmail.com>
2024-06-27 10:50:10 -0700

    feat(email component): Added custom form email component

--- client/components/FormInputComponents/Email/Email.tsx
+++ client/components/FormInputComponents/Email/Email.tsx
@@ -1,8 +1,45 @@
 import React from "react";
 import { FieldProps } from "../types";
+import { FieldError, useFormContext } from "react-hook-form";
+import styles from "./Email.module.css";
 
-const Email = ({ field }: FieldProps) => {
-  return <div>{JSON.stringify(field, null, 2)}</div>;
+const Email = ({ field, index }: FieldProps) => {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
+  const { required, max_length: maxLength } = field.validations ?? {};
+
+  const fieldError = (
+    errors.answers as { [key: number]: { email?: FieldError } } | undefined
+  )?.[index]?.email;
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.questionContainer}>
+        <h3 className={styles.question}>Question: {field.title}</h3>
+        {field.validations?.required && (
+          <span className={styles.required}>*</span>
+        )}
+      </div>
+      {fieldError && (
+        <span className={styles.errorMessage}>{fieldError.message}</span>
+      )}
+      <input
+        className={styles.input}
+        type="email"
+        placeholder="name@example.com"
+        {...register(`answers.${index}.email`, {
+          required: required && "This field is required",
+          maxLength: maxLength && {
+            value: maxLength,
+            message: `Answer has to be less than ${maxLength} characters`,
+          },
+        })}
+      />
+    </section>
+  );
 };
 
 export default Email;
