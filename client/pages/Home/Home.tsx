6dd54c60c1a061580ddc5f1be2b9a04fc1819aaa
Jose Patino <120534408+jipatino98@users.noreply.github.com>
2024-07-01 12:29:29 -0700

    wip: Fixed opacity bug when clicking on share btn

--- client/pages/Home/Home.tsx
+++ client/pages/Home/Home.tsx
@@ -1,11 +1,11 @@
-// import Leagues from "../Leagues/Leagues";
-import Seasons from "../Seasons/Seasons";
+import Leagues from "../Leagues/Leagues";
+// import Seasons from "../Seasons/Seasons";
 import { useAuth } from "../../components/AuthContext/AuthContext";
 
 const Home = () => {
   const { currentUser } = useAuth();
 
-  return <div>{currentUser ? <Seasons /> : <></>}</div>;
+  return <div>{currentUser ? <Leagues /> : <></>}</div>;
 };
 
 export default Home;
