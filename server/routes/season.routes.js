c73c2767fb94e90cf6cd6da16f9eb5ea7614723e
Jesus Gomez <40707209+JesusGo115@users.noreply.github.com>
2024-07-09 15:38:02 -0700

    Response Display Issue Fix (#97)

--- server/routes/season.routes.js
+++ server/routes/season.routes.js
@@ -7,6 +7,7 @@ const DivisionController = require("../controllers/division.controller");
 const router = Router();
 router.get("/", SeasonController.getAllSeasons);
 router.get("/:id", SeasonController.getSeason);
+router.get("/:id/leagues", SeasonController.getSeasonByLeagueId);
 router.get("/:id/divisions", DivisionController.getBySeasonId);
 router.post("/", SeasonController.createSeason);
 router.patch("/:id", SeasonController.updateSeason);
