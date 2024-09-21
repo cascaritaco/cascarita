25013432f8bd48abd4f7884d552a52f493ca0266
Armando Banuelos <abanuelo@stanford.edu>
2024-09-21 13:02:01 -0700

    feat: implementing rudimentary logout functionality

--- client/components/LogoutButton/LogoutButton.tsx
+++ client/components/LogoutButton/LogoutButton.tsx
@@ -1,23 +1,24 @@
 import { Button } from "@radix-ui/themes";
 import { LogoutButtonProps } from "./types";
 import styles from "./LogoutButton.module.css";
-// import { useAuth0 } from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom"; // Import useNavigate hook
 
 const LogoutButton: React.FC<LogoutButtonProps> = ({ icon, label }) => {
   // const { logout } = useAuth();
-  const navigate = useNavigate();
+  const { logout } = useAuth0();
+  // const navigate = useNavigate();
 
-  const handleLogout = async () => {
-    try {
-      // await logout();
-      navigate("/login");
-    } catch (error) {
-      console.error("Logout failed", error);
-    }
-  };
+  // const handleLogout = async () => {
+  //   try {
+  //     // await logout();
+  //     navigate("/login");
+  //   } catch (error) {
+  //     console.error("Logout failed", error);
+  //   }
+  // };
   return (
-    <Button onClick={handleLogout} variant="soft" className={styles.button}>
+    <Button onClick={() => logout()} variant="soft" className={styles.button}>
       <span className={styles.icon}>{icon}</span>
       <span className={styles.label}>{label}</span>
     </Button>
--- client/pages/Home/Home.tsx
+++ client/pages/Home/Home.tsx
@@ -1,12 +1,18 @@
 import Leagues from "../Leagues/Leagues";
 import { useAuth0 } from "@auth0/auth0-react";
-// import { useEffect } from "react";
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 const Home = () => {
-  const { isAuthenticated } = useAuth0();
+  const { user, isAuthenticated } = useAuth0();
   // const history = useNavigate(); // For react-router v6, use useNavigate instead.
 
+  // At the time we are authenticated we want to store this users data info
+  useEffect(() => {
+    if (isAuthenticated) {
+      console.log("user from AuthZero: ", user);
+    }
+  }, [isAuthenticated]);
   // useEffect(() => {
   //   if (!isLoading && !isAuthenticated) {
   //     // If not authenticated, redirect the user to the login page
