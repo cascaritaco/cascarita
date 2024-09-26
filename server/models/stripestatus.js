ac155c9a27f7118acc43d0db81b239774431033b
Saul Reyes <reyessaul796@gmail.com>
2024-09-26 14:37:18 -0700

    feat: new migration and model update to link stripe status to stripe user accounts

--- server/models/stripestatus.js
+++ server/models/stripestatus.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      StripeStatus.hasMany(models.UserStripeAccounts, {
+        foreignKey: "stripe_status_id",
+        sourceKey: "id",
+      });
     }
   }
   StripeStatus.init(
