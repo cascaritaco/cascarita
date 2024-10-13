418d762d3a3a6ebd7857f876016f22e72e60fe13
Saul Reyes <reyessaul796@gmail.com>
2024-10-13 15:30:04 -0700

    feat: basic webhook set up to handle form transactions

--- server/controllers/stripeResources/connectedAccount.controller.js
+++ server/controllers/stripeResources/connectedAccount.controller.js
@@ -86,6 +86,7 @@ const ConnectAccountController = function () {
 
   return {
     handleEvent,
+    verifyRequest,
   };
 };
 
--- server/routes/webhooks/stripe.webhooks.js
+++ server/routes/webhooks/stripe.webhooks.js
@@ -2,8 +2,11 @@
 
 const express = require("express");
 const ConnectedAccountController = require("../../controllers/stripeResources/connectedAccount.controller");
+const FormTransactionController = require("../../controllers/stripeResources/formTransaction.controller");
+
 const router = express.Router();
 
 router.post("/connect", ConnectedAccountController.handleEvent);
+router.post("/form-transaction", FormTransactionController.handleEvent);
 
 module.exports = router;
