c3a5ab7c2d6e3052977159d005dfa6d2f00acc8c
alonso <aortiz.dev@outlook.com>
2024-10-17 06:28:41 -0700

    feat: Add search bar and filter users

--- client/pages/Users/Users.tsx
+++ client/pages/Users/Users.tsx
@@ -14,6 +14,9 @@ import styles from "./Users.module.css"
 import DashboardTable from "../../components/DashboardTable/DashboardTable";
 import DropdownMenuButton from "../../components/DropdownMenuButton/DropdownMenuButton";
 import { User } from "./types";
+import Search from "../../components/Search/Search";
+import { TbSettingsSearch } from "react-icons/tb";
+
 
 const Users = () => {
   // Confligure translation
@@ -21,7 +24,9 @@ const Users = () => {
 
   // State variables
   const { currentUser } = useAuth();
-  const [users, setUsers] = useState<User[]>([]);
+  const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [debouncedQuery, setDebouncedQuery] = useState("");
 
   const mapRoles = (role_id: number) => {
     switch (role_id) {
@@ -32,24 +37,53 @@ const Users = () => {
 
   const groupId = currentUser?.group_id;
 
-  const { data, isLoading, isError } = useQuery({
+  const { data: users, isLoading, isError } = useQuery({
     queryKey: ["users", groupId ? groupId : 0],
     queryFn: getUsersByGroupId,
   });
 
+  // useEffect(() => {
+  //   const filteredData = data?.filter((user: User) => user.email !== currentUser?.email);
+  //   setUsers(filteredData);
+  // }, [data]);
+
   useEffect(() => {
-    const filteredData = data?.filter((user: User) => user.email !== currentUser?.email);
-    setUsers(filteredData);
-  }, [data]);
+    const handleDebounce = setTimeout(() => {
+      setDebouncedQuery(searchQuery);
+    }, 300);
+
+    return () => {
+      clearTimeout(handleDebounce);
+    };
+  }, [searchQuery]);
+
+  useEffect(() => {
+    if (users) {
+      const filteredData = users.filter((user: User) => {
+        const fullName = `${user.first_name.toLowerCase()} ${user.last_name.toLowerCase()}`;
+        const query = debouncedQuery.toLowerCase();
+        return (
+          fullName.includes(query) ||
+          user.email.toLowerCase().includes(query)
+        ) && user.email !== currentUser?.email; // Exclude current user
+      });
+      setFilteredUsers(filteredData);
+    }
+  }, [debouncedQuery, users]);
 
   return (
     <Page title={t("title")}>
+      <div className={styles.filterSearch}>
+        <div className={styles.dropdown}>
+          <Search onSearchChange={setSearchQuery} />
+        </div>
+      </div>
       <DashboardTable
         headers={["Name", "Email", "Role", "Options"]}
         headerColor="light"
       >
 
-        {users?.map((user, idx) => (
+        {filteredUsers?.map((user: User, idx: number) => (
           <tr key={idx} className={styles.tableRow}>
             <td className={`${styles.tableData} ${styles.leadingColumn}`}>
               <Avatar.Root className="AvatarRoot">
