86268098ae7128b325b54c380778c65505a97b74
Armando Bañuelos <32311654+abanuelo@users.noreply.github.com>
2024-11-07 23:35:28 -0800

    feat: add AWS SDK to create .env file (#133)
    Co-authored-by: Eli Manzo <96manzo.eli@gmail.com>

---
+++ create-env.js
@@ -0,0 +1,55 @@
+const {
+  SecretsManagerClient,
+  GetSecretValueCommand,
+} = require("@aws-sdk/client-secrets-manager");
+const { fromIni } = require("@aws-sdk/credential-provider-ini");
+const fs = require("fs");
+
+// const secretName = "dev/env";
+const environment = process.argv[2] || "dev";
+console.log(`Creating .env file for environment: ${environment}`);
+const secretName = `${environment}/env`;
+const region = "us-west-1";
+const envFilePath = ".env";
+const profile = "cascarita";
+
+const client = new SecretsManagerClient({
+  region,
+  credentials: fromIni({ profile }),
+});
+
+async function createEnvFile() {
+  try {
+    const response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secretName,
+        VersionStage: "AWSCURRENT",
+      }),
+    );
+
+    const secretString = response.SecretString;
+
+    if (!secretString) {
+      throw new Error("SecretString is Empty");
+    }
+    
+    const secretObject = JSON.parse(secretString);
+
+    const envFileContent = Object.entries(secretObject)
+      .map(([key, value]) => `${key}=${value}`)
+      .join("\n");
+
+    fs.writeFileSync(envFilePath, envFileContent);
+
+    console.log(
+      `.env file created at ${envFilePath} with secrets from AWS Secrets Manager.`,
+    );
+  } catch (error) {
+    console.error("Error fetching secret:", error);
+    throw error;
+  }
+}
+
+createEnvFile().catch((err) => {
+  console.error("Failed to create .env file:", err);
+});
